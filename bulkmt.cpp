9aff202d161c26c2d745ad79d18d7691b5c8ef39
Albert Kharisov <ah@bright-box.com>
2019-01-30 00:33:17 +0300

    it does something

--- bulkmt.cpp
+++ bulkmt.cpp
@@ -35,45 +35,51 @@
 
 using vector_string = std::vector<std::string>;
 
+struct worker
+{
+    std::thread thread;
+    std::string name;
+    worker(std::thread t, std::string &s) : thread(std::move(t)), name(s) {}
+    worker(std::thread t, const char *s)  : thread(std::move(t)), name(s) {}
+};
 
 class IbaseClass
 {
 public:
     using type_to_handle = struct {
-    // send by copy, not by ref, because we do things asynchronically
-    // and main thread is cleared vector_string right after thread
-    // notification. Or it should wait all other tasks, which is
-    // quite dummy in multithread system.
         const vector_string vs;
         const std::time_t t;
-    };
-    using worker_type = struct {
-        std::thread thrd;
-        std::string thrd_name;
-
     };
     std::condition_variable cv;
     std::mutex cv_m;
     std::queue<type_to_handle> qMsg;
-
-    std::vector<std::thread> vThread;
-    const size_t threads;
+    std::vector<worker> vThread;
+    bool exit;
 
     template<typename ...Names>
-    IbaseClass(Names... names) : threads(sizeof...(Names)) {
-//        int dummy[sizeof...(Names)] = { (std::cout << names, 0)... };
-        type
+    IbaseClass(Names... names) : exit(false)
+    {
+        const char * dummy[] = {(const char*)(names)...};
+        vThread.reserve(sizeof...(Names));
+        auto it = vThread.begin();
+        for (auto &s : dummy)
+            vThread.emplace(it++, std::thread(), s);
     }
-//    IbaseClass(size_t threads = 1) : threads(threads) {}
+
     virtual ~IbaseClass(void)
     {
-        std::cout << "virtual ~IbaseClass()" << std::endl;
+        std::cout << "virtual ~IbaseClass() : join..." << std::endl;
+        exit = true;
+        for (auto &a : vThread) {
+            a.thread.join();
+        }
+        std::cout << "virtual ~IbaseClass() : all joined" << std::endl;
     }
 
     void start_threads(void)
     {
-        for (size_t i = 0; i < threads; ++i)
-            vThread.push_back(std::thread(&IbaseClass::work, this));
+        for (auto &t : vThread)
+            t.thread = std::thread(&IbaseClass::work, this, std::ref(t.name));   // move thread is OK
     }
 
     void notify(type_to_handle &ht)
@@ -87,17 +93,25 @@ public:
         (void) ht;
         throw;
     }
-    void work(void)
+    void work(std::string &name)
     {
-        std::cout << std::this_thread::get_id() << " thread started! " << std::endl;
-        while(1) {
+        {
+            std::lock_guard<std::mutex>l(cv_m);
+            std::cout << name << " thread started! " << std::endl;
+        }
+        while(!exit) {
             std::unique_lock<std::mutex> lk(cv_m);
-            cv.wait(lk, [this](){ return !this->qMsg.empty(); });
+            cv.wait(lk, [this](){ return !this->qMsg.empty() || exit; });
+            if (exit) break;
             auto m = qMsg.front();
             qMsg.pop();
             lk.unlock();
-            std::cout << std::this_thread::get_id() << " thread handled! " << std::endl;
+            std::cout << name << " thread handled! " << std::endl;
             handle(m);
+        }
+        {
+            std::lock_guard<std::mutex>l(cv_m);
+            std::cout << name << " thread exit! " << std::endl;
         }
     }
 
@@ -124,7 +138,8 @@ using namespace std::chrono;
 class saver : public IbaseClass
 {
 public:
-    saver(size_t threads = 1) : IbaseClass(threads) {}
+    template<typename ...Names>
+    saver(Names... names) : IbaseClass(names...) {}
     void handle(const type_to_handle &ht) override
     {
         std::hash<std::thread::id> hash_thread_id;
@@ -259,10 +274,16 @@ void start_threads(void)
 
 int main(int argc, char ** argv)
 {
+    (void) argc;
+    (void) argv;
     std::srand(time(NULL));
 
-    IbaseClass base("123 ", "456");
-    printer printerHandler("111", "222");
+    saver saverHandler("_saver1", "_saver2", "_saver3");
+    printer printerHandler("_print");
+
+    printerHandler.start_threads();
+    saverHandler.start_threads();
+    std::this_thread::sleep_for(std::chrono::milliseconds(3000));
     return 0;
 
 #if 0
