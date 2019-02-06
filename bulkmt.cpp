538c849c4dbd253b726f0b8ee724766e7a9f781e
Albert Kharisov <ah@bright-box.com>
2019-02-06 22:39:56 +0300

    end test

--- bulkmt.cpp
+++ bulkmt.cpp
@@ -6,7 +6,7 @@
 #include <ctime>
 #include <string>
 #include <signal.h>
-#include "terminator.h"
+//#include "terminator.h"
 #include <stdexcept>
 
 #include <chrono>
@@ -14,6 +14,8 @@
 #include <queue>
 #include <thread>
 #include <atomic>
+
+#define BASE_TERMINATE  1
 
 struct worker
 {
@@ -58,7 +60,9 @@ public:
     virtual ~IbaseClass(void)
     {
         std::cout << "~IbaseClass()" << std::endl;
-        //terminate();
+#if BASE_TERMINATE == 1
+        terminate();
+#endif
         std::cout << "~IbaseClass() end" << std::endl;
     }
 
@@ -80,30 +84,57 @@ public:
         std::cout << " thread started! " << std::endl;
         while(1) {
             std::unique_lock<std::mutex> lk(cv_m);
-            cv.wait(lk, [this](){ return !this->qMsg.empty() || exit; });
+            cv.wait(lk, [this]() { return !this->qMsg.empty() || exit; });
             if (exit && qMsg.empty()) break;
             std::cout << " got job! " << std::endl;
             auto m = qMsg.front();
             qMsg.pop();
             lk.unlock();
-            std::cout << " thread handled! " << std::endl;
-            this->handle(m);
+            std::cout << " thread handled! this = " << this << std::endl;
+
+            size_t **ptr = reinterpret_cast<size_t **>(this);
+            for (int i = 0; i < 5; ++i) {
+                std::cout << i << " : " << ptr[i] << std::endl;
+            }
+            std::cout << std::endl;
+            size_t **ptrT = (size_t **) ptr[0];
+            for (int i = 0; i < 5; ++i) {
+                std::cout << &ptrT[i] << " : " << ptrT[i] << std::endl;
+            }
+
+            std::cout << " thread handled! this = " << this << std::endl;
+            if (this)
+                this->handle(m);
+            std::cout << " end of loop " << this << std::endl;
         }
         std::cout << " ended! " << std::endl;
     }
 };
 
-class printer : public IbaseClass
+struct printer2 : public IbaseClass
 {
-public:
+    void handle(int t) override { std::cout << "tlen" << std::endl;}
+    ~printer2() {
+        std::cout << "~printer()" << std::endl;
+#if BASE_TERMINATE == 0
+        terminate();
+#endif
+    }
+};
+
+struct printer : public IbaseClass
+{
     printer() {}
     void handle(int t) override
     {
+        std::cout << "handle" << std::endl;
         std::cout << t << std::endl;
     }
     ~printer() {
         std::cout << "~printer()" << std::endl;
+#if BASE_TERMINATE == 0
         terminate();
+#endif
     }
 };
 
@@ -112,9 +143,17 @@ public:
 int main()
 {
     try {
+        {
+        printer2 printerHandler2{};
+        printerHandler2.start_threads();
+        printerHandler2.notify(15);
+        std::cout << &printerHandler2 << std::endl;
+        }
+    std::this_thread::sleep_for(std::chrono::milliseconds(300));
         printer printerHandler{};
         printerHandler.start_threads();
         printerHandler.notify(15);
+        std::cout << &printerHandler << std::endl;
     }
     catch (std::exception &e) { std::cout << "CATCH WHAT???  " << e.what() << std::endl; }
     return 0;
