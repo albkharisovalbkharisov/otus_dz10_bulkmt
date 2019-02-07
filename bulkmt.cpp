e833a3ab040b6133f9d9d071e73bc818bb2c636f
Albert Kharisov <ah@bright-box.com>
2019-02-07 13:28:26 +0300

    test

--- bulkmt.cpp
+++ bulkmt.cpp
@@ -1,75 +1,43 @@
 #include <cstdlib>
 #include <iostream>
-#include <vector>
-#include <list>
-#include <fstream>
-#include <ctime>
 #include <string>
-#include <signal.h>
-//#include "terminator.h"
 #include <stdexcept>
-
-#include <chrono>
 #include <condition_variable>
 #include <queue>
 #include <thread>
 #include <atomic>
 
-#define BASE_TERMINATE  1
+#define BASE_TERMINATE  0
 
-struct worker
-{
-    std::thread thread;
-//    worker(std::thread t) : thread(std::move(t)) {}
-    worker(std::thread t) : thread(std::move(t)) {}
-    ~worker(void) { std::cout << "~worker()" << std::endl; }
-};
-
-class IbaseClass
+class base
 {
 public:
     std::condition_variable cv;
+    std::atomic<bool> exit;
     std::mutex cv_m;
     std::queue<int> qMsg;
-    std::list<worker> vThread;
-    std::atomic<bool> exit;
-    virtual void handle(int t) { (void) t; throw; }
+    std::thread thread;
+    virtual void eat() { std::cout << "EAT BASE" << std::endl; }
 
-    IbaseClass() : exit(false)
-    {
-        vThread.emplace_back(std::thread());
-    }
+    base() : exit(false), thread(std::thread(&base::work, this)) {}
 
     void terminate(void)
     {
-        std::cout << "terminate()" << std::endl;
         exit = true;
-        std::cout << "terminate() : notify..." << std::endl;
         cv.notify_all();
-        for (auto &a : vThread) {
-            if (a.thread.joinable()){
-                std::cout << "terminate() : join..." << std::endl;
-                a.thread.join();
-            }
-            else
-                std::cout << "terminate() : join..." << " can't!" << std::endl;
+        if (thread.joinable()){
+            thread.join();
         }
-        std::cout << "terminate() : all joined" << std::endl;
+        else
+            std::cout << "terminate() : join..." << " can't!" << std::endl;
     }
 
-    virtual ~IbaseClass(void)
+    virtual ~base(void)
     {
-        std::cout << "~IbaseClass()" << std::endl;
+        std::cout << "~base()" << std::endl;
 #if BASE_TERMINATE == 1
         terminate();
 #endif
-        std::cout << "~IbaseClass() end" << std::endl;
-    }
-
-    void start_threads(void)
-    {
-        for (auto &w : vThread)
-            w.thread = std::thread(&IbaseClass::work, this, std::ref(w));   // moving thread is OK
     }
 
     void notify(int t)
@@ -78,82 +46,41 @@ public:
         cv.notify_one();
     }
 
-    void work(struct worker &w)
+    void work()
     {
-        (void) w;
         std::cout << " thread started! " << std::endl;
         while(1) {
             std::unique_lock<std::mutex> lk(cv_m);
             cv.wait(lk, [this]() { return !this->qMsg.empty() || exit; });
             if (exit && qMsg.empty()) break;
-            std::cout << " got job! " << std::endl;
-            auto m = qMsg.front();
+            qMsg.front();
             qMsg.pop();
             lk.unlock();
-            std::cout << " thread handled! this = " << this << std::endl;
-
-            size_t **ptr = reinterpret_cast<size_t **>(this);
-            for (int i = 0; i < 5; ++i) {
-                std::cout << i << " : " << ptr[i] << std::endl;
-            }
-            std::cout << std::endl;
-            size_t **ptrT = (size_t **) ptr[0];
-            for (int i = 0; i < 5; ++i) {
-                std::cout << &ptrT[i] << " : " << ptrT[i] << std::endl;
-            }
-
-            std::cout << " thread handled! this = " << this << std::endl;
             if (this)
-                this->handle(m);
-            std::cout << " end of loop " << this << std::endl;
+                this->eat();        // THE GIST ! THE POINT! THE CORE!
         }
-        std::cout << " ended! " << std::endl;
     }
 };
 
-struct printer2 : public IbaseClass
+struct derived : public base
 {
-    void handle(int t) override { std::cout << "tlen" << std::endl;}
-    ~printer2() {
-        std::cout << "~printer()" << std::endl;
+    void eat() override {
+        std::cout << "EAT DERIVED" << std::endl;
+    }
+    ~derived() {
+        std::cout << "~derived()" << std::endl;
 #if BASE_TERMINATE == 0
         terminate();
 #endif
     }
 };
-
-struct printer : public IbaseClass
-{
-    printer() {}
-    void handle(int t) override
-    {
-        std::cout << "handle" << std::endl;
-        std::cout << t << std::endl;
-    }
-    ~printer() {
-        std::cout << "~printer()" << std::endl;
-#if BASE_TERMINATE == 0
-        terminate();
-#endif
-    }
-};
-
 
 
 int main()
 {
     try {
-        {
-        printer2 printerHandler2{};
-        printerHandler2.start_threads();
-        printerHandler2.notify(15);
-        std::cout << &printerHandler2 << std::endl;
-        }
-    std::this_thread::sleep_for(std::chrono::milliseconds(300));
-        printer printerHandler{};
-        printerHandler.start_threads();
-        printerHandler.notify(15);
-        std::cout << &printerHandler << std::endl;
+        derived test{};
+        test.notify(15);
     }
     catch (std::exception &e) { std::cout << "CATCH WHAT???  " << e.what() << std::endl; }
     return 0;
